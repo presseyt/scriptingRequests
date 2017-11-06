1e4183cf678cb16d664c991a0798710392454761
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-06 16:08:26 +0000

    sent a request and recieved chunks of data

---
+++ getAndPrintHTMLChunks.js
@@ -0,0 +1,25 @@
+
+
+(function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  var https = require('https');
+
+  https.get(requestOptions, function(res){
+    //console.log(res.statusCode);   //200 OK
+
+    res.setEncoding('utf8');
+
+    var rawData = '';
+
+    res.on('data', function(chunk){
+      console.log(chunk + '\n');
+    });
+
+  });
+
+})();
